2655b2bf72cefe8e90ee9e122c724bf48bf29189
meeehdi <mehdi.mhiri@gmail.com>
2023-09-03 14:06:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# Win-picker.nvim
+
+Win-picker allows you to quickly focus any window in your tabpane.
+
+It is inspired by the default window picker of [nvim-tree](https://github.com/nvim-tree/nvim-tree.lua).
+
+## Configuration
+
+Here is the default configuration.
+
+- `chars` is a string defining the characters used to select the window.
+- `filter` is a function receiving a win_id as parameter and returning a boolean to filter out a window.
+- `hl_current` is either a boolean to highlight or not the current window using the hl_group or a string designating an other predefined highlight group.
+- `hl_group` is a string designating a predefined highlight group.
+- `hl` defines a new hl group.
+
+```lua
+require('win-picker').setup({
+  chars = "1234567890",
+  filter = nil,
+  hl_current = false,
+  hl_group = nil,
+  hl = {
+    group = "WinPicker",
+    gui = "bold",
+    guifg = "#1d202f",
+    guibg = "#7aa2f7",
+  },
+})
+```
+
+## Usage
+
+Install and configure using [lazy.nvim](https://github.com/folke/lazy.nvim)
+```lua
+  return {
+    'meeehdi-dev/win-picker.nvim',
+    config = true, -- setup with default options
+    keys = {
+      {
+        "<C-w>!" = function()
+          local win_id = require("win-picker").pick_win()
+          if win_id then
+            vim.api.nvim_set_current_win(win_id)
+          end
+        end,
+      },
+    },
+  },
+  -- or
+  return {
+    'meeehdi-dev/win-picker.nvim',
+    opts = {
+      chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+      filter = function(id)
+        local bufid = vim.api.nvim_win_get_buf(id)
+        local ft = vim.api.nvim_buf_get_option(bufid, "filetype")
+        return not vim.tbl_contains({"noice", "notify"}, ft)
+      end,
+      hl_current = "CustomHlGroupFromMyAwesomeTheme",
+      hl_group = "lualine_a_normal", -- use lualine normal mode hl group
+      -- hl = {
+      --     group = "NvimTreeWinPickerStyle",
+      --     gui = "bold",
+      --     guifg = "#ededed",
+      --     guibg = "#4493c8",
+      -- },
+    },
+    keys = {
+      {
+        "<leader>w" = function()
+          local win_id = require("win-picker").pick_win()
+          if win_id then
+            vim.api.nvim_set_current_win(win_id)
+          end
+        end,
+      },
+    },
+  },
+```
+
+It can also be used as the window picker for nvim-tree
+```lua
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      {
+        "meeehdi-dev/win-picker.nvim",
+        config = true,
+      },
+    },
+    opts = {
+      actions = {
+        open_file = {
+          window_picker = {
+            picker = function()
+              return require("win-picker").pick_win()
+            end,
+          },
+        },
+      },
+    },
+    -- ...
+  },
+```
+
---
+++ lua/win-picker/init.lua
@@ -0,0 +1,102 @@
+local M = {}
+
+local backed_up_opts = {"winhl", "statusline"}
+
+local function backup_win_opts(win_id)
+  local win_opts = {}
+  for _, opt in ipairs(backed_up_opts) do
+    win_opts[opt] = vim.api.nvim_win_get_option(win_id, opt)
+  end
+  return win_opts
+end
+
+local function restore_win_opts(win_id, win_opts)
+  for opt, value in pairs(win_opts) do
+    vim.api.nvim_win_set_option(win_id, opt, value)
+  end
+end
+
+M.pick_win = function()
+  local tabpage = vim.api.nvim_get_current_tabpage()
+  local win_ids = vim.api.nvim_tabpage_list_wins(tabpage)
+  if M.opts.filter ~= nil then
+    win_ids = vim.tbl_filter(M.opts.filter, win_ids)
+  end
+
+  if #win_ids == 0 then
+    vim.notify("No windows to pick from", vim.log.levels.ERROR)
+    return nil
+  end
+  if #win_ids == 1 then
+    vim.notify("Only one window", vim.log.levels.ERROR)
+    return nil
+  end
+  if #M.opts.chars < #win_ids then
+    vim.notify("Too many windows to pick from (Update `chars`)", vim.log.levels.ERROR)
+    return nil
+  end
+
+  local win_opts = {}
+  local win_map = {}
+  local laststatus = vim.o.laststatus
+  vim.o.laststatus = 2
+
+  for i, id in ipairs(win_ids) do
+    local char = M.opts.chars:sub(i, i)
+    win_opts[id] = backup_win_opts(id)
+    win_map[char] = id
+
+    vim.api.nvim_win_set_option(id, "statusline", "%=" .. char .. "%=")
+    if vim.api.nvim_get_current_win() ~= id then
+      vim.api.nvim_win_set_option(id, "winhl", "StatusLine:" .. M.opts.hl_group .. ",StatusLineNC:" .. M.opts.hl_group)
+    elseif M.opts.hl_current ~= false then
+      local hl_group = M.opts.hl_current == true and M.opts.hl_group or M.opts.hl_current
+      vim.api.nvim_win_set_option(id, "winhl", "StatusLine:" .. hl_group .. ",StatusLineNC:" .. hl_group)
+    end
+  end
+
+  vim.cmd.redraw()
+  -- wait for a valid input
+  local c = vim.fn.getchar()
+  while type(c) ~= "number" do
+    c = vim.fn.getchar()
+  end
+  local resp = (vim.fn.nr2char(c) or ""):upper()
+
+  for _, id in ipairs(win_ids) do
+    restore_win_opts(id, win_opts[id])
+  end
+
+  vim.o.laststatus = laststatus
+
+  if not vim.tbl_contains(vim.split(M.opts.chars, ""), resp) then
+    vim.notify("Invalid input", vim.log.levels.ERROR)
+    return nil
+  end
+
+  return win_map[resp]
+end
+
+M.opts = {
+  chars = "1234567890",
+  filter = nil,
+  hl_current = false,
+  hl_group = nil,
+  hl = {
+    group = "WinPicker",
+    gui = "bold",
+    guifg = "#1d202f",
+    guibg = "#7aa2f7",
+  },
+}
+
+function M.setup(opts)
+  M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+
+  if M.opts.hl_group == nil then
+    M.opts.hl_group = M.opts.hl.group
+    vim.api.nvim_command("hi def " .. M.opts.hl_group .. " gui=" .. M.opts.hl.gui .. " guifg=" .. M.opts.hl.guifg .. " guibg=" .. M.opts.hl.guibg)
+  end
+end
+
+return M
